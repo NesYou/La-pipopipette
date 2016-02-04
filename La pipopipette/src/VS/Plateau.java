298ed7124eb48e539d1f98be54d1f4922c711a3b
Younes <younes.bouayadine@gmail.com>
2016-02-04 16:07:50 +0100

    Ajout classe plateau

---
+++ La pipopipette/src/VS/Plateau.java
@@ -0,0 +1,16 @@
+package VS;
+
+public class Plateau {
+	
+	private int [][] ligne;
+	private int [][] colone;
+	
+	public Plateau(int hLigne, int lLigne, int hColone, int lColone) {
+		this.ligne = new int[hLigne][lLigne];
+		this.colone = new int[hColone][lColone];
+		
+	}
+	
+	
+
+}
