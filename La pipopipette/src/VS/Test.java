749a7d09100e7eb9fe1f6c91d056ddb7e0f64165
Younes <younes.bouayadine@gmail.com>
2016-02-25 20:13:52 +0100

    Mise à jour du classpath : 	- Classe Plateau passe du package default -> VS 	- Suppression de la classe HelloWorld, contenue déplacé dans la classe Test (Package VS) 	- Package default supprimé car vide.
    Signed-off-by: Younes <younes.bouayadine@gmail.com>

--- La pipopipette/src/VS/Test.java
+++ La pipopipette/src/VS/Test.java
@@ -1,19 +1,13 @@
 package VS;
 
 public class Test {
-	
-	public static void main(String [] args) {
-		
-		int[][] tab = new int[10][10];
-		
-		for(int i = 0; i < 9; i++) {
-			for(int j = 0; j < 9; j++) {
-				tab[i][j] = 12;
-				System.out.print(tab[i][j]);
-			}
-			System.out.println();
-		}
-		
+
+	public static void main(String[] args) {
+
+		Plateau plateau = new Plateau(3, 3, true);
+		System.out.println(plateau.toString());
+		// System.out.println(plateau.caractere());
+
 	}
 
 }
