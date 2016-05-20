bf6a34673d3396770710ea06d676d4c7ecef3bcf
Younes <younes.bouayadine@gmail.com>
2016-05-20 20:18:03 +0200

    Les derniers commits
    Signed-off-by: Younes <younes.bouayadine@gmail.com>

--- La pipopipette/src/graph/Configurations.java
+++ La pipopipette/src/graph/Configurations.java
@@ -1,31 +1,81 @@
 package graph;
 
-import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import settings.Dynamic;
+import settings.State;
+import strategy.Simple;
 
 /**
  * 
- * @author elsacollet
+ * @author Elsa Collet
  *
- * @param <K>
- * @param <V>
- * Permet d'éviter de faire les mêmes calcules
+ * Classe qui genere le graphe des configurations reduit d'une strategie s passee en parametre
  */
-public class Configurations<K,V> implements Function<K,V>{
-	HashMap<K,V> map;
-	Function<K,V> f;
-	
-	public Configurations(Function<K,V> f1){
-		this.map=new HashMap<K, V>();
-		this.f=f1;
+public class Configurations{
+	State root;
+	Dynamic<State, Iterable<State>> g = new Dynamic<>();
+	HashMap<State,Integer> id;
+	PrintWriter sc;
+
+
+	public Configurations(String title, String fileName) throws FileNotFoundException{
+		g = new Simple();
+		this.root = new State();
+		this.id = new HashMap<>();
+		sc= new PrintWriter(fileName);
+		generate(true, title);
+
 	}
-	
-	@Override
-	public V get(K k) {
-		V v = map.get(k);
-		if(v==null){
-			v=f.get(k);
-			map.put(k, v);
+
+	public String successors(State s){
+		String sc="";
+		String display = "";
+		s.completeSquare();
+
+		if(!id.containsKey(s) ){
+			id.put(s, id.size());
+			s.setName(id.get(s));
+			sc += s.toDot( s.getWeight() );
+			if(!s.gameOver()){
+				Iterable<State> i = (Iterable<State>) g.get(s);
+				for(State s_: i){
+					display+=successors( s_);
+					try{
+						Pattern p = Pattern .compile("a*b|c");
+						String entree = "aabbbcab";
+						Matcher m = p.matcher(entree);
+						while (m.find())
+							System.out.println(entree.substring(m.start(), m.end()));
+					}catch(PatternSyntaxException pse){
+					}
+					display+= "N"+s.getName() + " -> N"+s_.getName() + ";\n";
+				}
+			}
+			return sc + display;
 		}
-		return v;
+		s.setName(id.get(s));
+		return sc + display;
+	}
+
+
+
+	public void generate(boolean b, String title){
+		id = new HashMap<>();
+		String display="digraph pipopipetteStrategie {\n"
+				+ "graph[labelloc=\"t\"  fontsize=16 fontcolor=\"blue\""
+				+ "label=\""+ title +" \" ]\n"
+				+ "node [shape=box fontname = \"Courier New\" color=\"sienna\"]\n"
+				+ "edge [fontname = \"Times\" fontcolor=\"sienna\"]\n\n";
+
+		display +=successors(root);
+		sc.write(display);
+		sc.write("\n }");
+		sc.close();
 	}
 }
--- La pipopipette/src/jUnit/MatrixTest.java
+++ La pipopipette/src/jUnit/MatrixTest.java
@@ -1,7 +1,253 @@
 package jUnit;
 
-import static org.junit.Asset.*;
+import junit.framework.TestCase;
+import settings.Board;
+import settings.DefaultSettings;
+import settings.Matrix;
+import settings.Pair;
 
-public class MatrixTest {
+/**
+ * 
+ * @author Moussi C�dric
+ * Test unitaire de la classe Matrix
+ **/
+
+public class MatrixTest extends TestCase {
+	public int mat[][];
+
+
+	public void testMatrix() {
+		Matrix mat = new MatrixImpl();
+		DefaultSettings.rows = 4;
+		DefaultSettings.cols = 5;
+		this.mat = new int[DefaultSettings.rows * 2+1][DefaultSettings.cols * 2+1];
+		for(int j=0; j<=DefaultSettings.rows*2; j++){
+			for(int i=0; i<= DefaultSettings.cols * 2; i++){
+				//Présence d'un nombre ou d'un point ?
+				if(j%2 == 0){
+					if(i%2==0){
+						this.mat[j][i]=2;
+					}else{
+						if(DefaultSettings.border){
+							if(j==0 || j==DefaultSettings.rows * 2){
+								this.mat[j][i]=1;
+							}else{
+								this.mat[j][i]=0;
+							}
+						}else{
+							this.mat[j][i]=0;
+						}
+					}
+				}else{
+					if(i%2==0){
+						if(DefaultSettings.border){
+							if(i==0 || i==DefaultSettings.cols * 2){
+								this.mat[j][i]=1;
+							}else{
+								this.mat[j][i]=0;
+							}
+						}else{
+							this.mat[j][i]=0;
+						}
+					}else{
+						this.mat[j][i]=2;
+					}
+				}
+			}
+		}
+	}
+
+	public void testAddMvn() {
+		int motion = 2;
+		try {
+			Pair p = new Pair();
+			p = p.convertToMap(motion, mat);
+			mat[p.getFst()][p.getSnd()]=1;
+		}
+		catch ( Exception e)
+		{ 
+			System.out.println("La methode AddMvn a rencontree une erreur"); 
+		}
+	}
+
+	public void testMatrixBoard() {
+		Board board = new Board();
+		DefaultSettings.rows = 4;
+		DefaultSettings.cols = 3;
+		try {
+			if(board != null){
+				boolean val;
+				int h=0;
+				this.mat= new int[DefaultSettings.rows*2+1][DefaultSettings.cols * 2+1];
+				for(int j=DefaultSettings.rows * 2; j>= 0; j--){
+					for(int i=DefaultSettings.cols * 2; i>=0 ; i--){
+						//Présence d'un nombre ou d'un point ?
+						if(j%2 == 0){
+							if(i%2==0){
+								val=false;
+							}else{
+								val=true;
+							}
+						}else{
+							if(i%2==0){
+								val=true;
+							}else{
+								val=false;
+							}
+						}
+						if( !val){
+							//C'est un point
+							mat[j][i]=2;
+						}else{
+							if( board.isFull(h) ){
+								mat[j][i]=1;
+							}else{
+								mat[j][i]=0;
+							}
+							h++;
+						}
+					}
+				}
+			}
+		}
+		catch ( Exception e)
+		{ 
+			System.out.println ( "La methode MatrixBoard a rencontree une erreur"); 
+		}
+	}
+
+	public void testMatrixMatrix() {
+		Matrix m = new Matrix();
+		try {
+			this.mat = m.mat;
+		}
+		catch ( Exception e)
+		{
+			System.out.println (" La methode MatrixMatrix a rencontree une erreur");
+		}
+	}
+
+	public void testRotation() {
+		DefaultSettings.cols = 10;
+		DefaultSettings.rows = 5;
+		try {
+			int t[][] = new int [DefaultSettings.cols*2+1][DefaultSettings.rows * 2+1];
+			int r[][]=new int[DefaultSettings.cols*2+1][DefaultSettings.rows * 2+1];
+			for(int j=0;j<=DefaultSettings.cols*2;j++){
+				for(int i=0;i<=DefaultSettings.rows*2;i++){
+					r[j][i]=t[i][DefaultSettings.cols*2-j];
+				}
+			}
+		}
+		catch ( Exception e)
+		{
+			System.out.println("La methode Rotation a rencontree une erreur");
+		}
+	}
+
+	public void testRotationMatrice() {
+		try {
+			Matrix m=new MatrixImpl();
+			m.mat=m.rotation(this.mat);
+		}
+		catch ( Exception e)
+		{
+			System.out.println("La methode RotationMatrice a rencontree une erreur");
+		}
+	}
+
+	public void testSymAbscisse() {
+		try {
+			Matrix m = new MatrixImpl();
+			DefaultSettings.rows = 4;
+			DefaultSettings.cols = 2;
+			for(int j=0; j<=DefaultSettings.rows*2; j++){
+				for(int i=0; i<=DefaultSettings.cols*2; i++){
+					m.mat[j][i]=mat[DefaultSettings.rows*2-j][i];
+				}
+			}
+		}
+		catch ( Exception e)
+		{
+			System.out.println("Probl�me lors de la symetrie abscisse");
+		}
+	}
+
+	public void testSymOrdonnee() {
+		try {
+			Matrix m = new MatrixImpl();
+			DefaultSettings.rows = 4;
+			DefaultSettings.cols = 4;
+			for(int j=0; j<=DefaultSettings.rows*2; j++){
+				for(int i=0; i<=DefaultSettings.cols*2; i++){
+					m.mat[j][i]=this.mat[j][DefaultSettings.cols*2-i];
+				}
+			}
+		}
+		catch ( Exception e)
+		{
+			System.out.println("Probl�me lors de la symetrie ordonnee");
+		}
+	}
+
+
+	public void testFindSqr() {
+		try {
+
+			DefaultSettings.rows = 8;
+			DefaultSettings.cols = 8;
+			int mat[][] = new int [DefaultSettings.rows][DefaultSettings.cols];
+			for(int j=DefaultSettings.rows*2-1; j>0; j-=2){
+				for(int i=DefaultSettings.cols*2-1; i>0; i-=2){
+					if( this.mat[j][i]==2){
+						int val1= this.mat[j][i+1];
+						int val2= this.mat[j][i-1];
+						int val3= this.mat[j-1][i];
+						int val4= this.mat[j+1][i];
+						if( (val1==1 && val2==1 && val3==1 && val4==0)){
+							Pair p= new Pair(j+1, i);
+							return;
+						}
+						if((val1==1 && val2==1 && val3==0 && val4==1) ){
+							Pair p= new Pair(j-1, i);
+							return;
+						}
+						if((val1==1 && val2==0 && val3==1 && val4==1) ){
+							Pair p= new Pair(j, i-1);
+							return;
+						}
+						if((val1==0 && val2==1 && val3==1 && val4==1) ){
+							Pair p= new Pair(j, i+1);
+							return;
+						}
+					}
+				}
+			}
+		}
+		catch ( Exception e)
+		{
+			System.out.println("Erreur lors de la m�thode findSqr");
+		}
+	}
+
+
+	public void testRestLine() {
+		try { 
+			DefaultSettings.rows = 8;
+			DefaultSettings.cols = 6;
+			int n=0;
+			for(int j=DefaultSettings.rows*2; j>=0; j--){
+				for(int i = DefaultSettings.cols*2; i>=0; i--){
+					if(this.mat[j][i] ==0){
+						n++;
+					}
+				}
+			}
+		}
+		catch ( Exception e)
+		{
+			System.out.println("Erreur lors de la methode RestLine");
+		}
+	}
 
 }
